4cc347744a12f73ff89e8284555c441984088e68
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-16 12:22:02 +0300

    Fix eslint errors

--- script.js
+++ script.js
@@ -23,40 +23,40 @@ courseSec.addEventListener('click', () => {
 // ---- Teacher Info List ---- //
 const teacherInfoList = [
   {
-    imageURL: "src/profile-photo/david-mallan.jpg",
-    teacherName: "David J. Mallan",
-    teacherTitle: "Professor of Computer Science at Harvard University",
-    teacherDesc: "Known for teaching course CS50, which is the largest open-learning course at Harvard University and Yale.",
+    imageURL: 'src/profile-photo/david-mallan.jpg',
+    teacherName: 'David J. Mallan',
+    teacherTitle: 'Professor of Computer Science at Harvard University',
+    teacherDesc: 'Known for teaching course CS50, which is the largest open-learning course at Harvard University and Yale.',
   },
   {
-    imageURL: "src/profile-photo/Wes Box.jpg",
-    teacherName: "Wes Bos",
-    teacherTitle: "Full Stack JavaScript developer from Canada",
-    teacherDesc: "I'm a web developer with 23 years experience, teacher and speaker from Hamilton, Ontario 🇨🇦.",
+    imageURL: 'src/profile-photo/Wes Box.jpg',
+    teacherName: 'Wes Bos',
+    teacherTitle: 'Full Stack JavaScript developer from Canada',
+    teacherDesc: 'I\'m a web developer with 23 years experience, teacher and speaker from Hamilton, Ontario 🇨🇦.',
   },
   {
-    imageURL: "src/profile-photo/jon-kleinberg.jpg",
-    teacherName: "Jon M. Kleinberg",
-    teacherTitle: "Professor in the Computer Science Department at Cornell University",
-    teacherDesc: "His research focuses on issues at the interface of networks and information systems.",
+    imageURL: 'src/profile-photo/jon-kleinberg.jpg',
+    teacherName: 'Jon M. Kleinberg',
+    teacherTitle: 'Professor in the Computer Science Department at Cornell University',
+    teacherDesc: 'His research focuses on issues at the interface of networks and information systems.',
   },
   {
-    imageURL: "src/profile-photo/charles-leiserson.jpg",
-    teacherName: "Charles Leisers",
-    teacherTitle: "Professor of Computer Science and Engineering at MIT",
-    teacherDesc: "Parallel computing is tremendously underutilized in today’s computer systems, due to its complexity.",
+    imageURL: 'src/profile-photo/charles-leiserson.jpg',
+    teacherName: 'Charles Leisers',
+    teacherTitle: 'Professor of Computer Science and Engineering at MIT',
+    teacherDesc: 'Parallel computing is tremendously underutilized in today’s computer systems, due to its complexity.',
   },
   {
-    imageURL: "src/profile-photo/clifford-stein.jpg",
-    teacherName: "Clifford Seth Stein",
-    teacherTitle: "professor of industrial engineering and operations research at Columbia University",
-    teacherDesc: "Stein's research interests include the design and analysis of algorithms, combinatorial optimization.",
+    imageURL: 'src/profile-photo/clifford-stein.jpg',
+    teacherName: 'Clifford Seth Stein',
+    teacherTitle: 'professor of industrial engineering and operations research at Columbia University',
+    teacherDesc: 'Stein\'s research interests include the design and analysis of algorithms, combinatorial optimization.',
   },
   {
-    imageURL: "src/profile-photo/sal-khan.jpg",
-    teacherName: "Sal Khan",
-    teacherTitle: "Sal Khan is the founder and CEO of Khan Academy",
-    teacherDesc: "He is also the founder of Schoolhouse.world, Khan Lab School and Khan World School, all nonprofits.",
+    imageURL: 'src/profile-photo/sal-khan.jpg',
+    teacherName: 'Sal Khan',
+    teacherTitle: 'Sal Khan is the founder and CEO of Khan Academy',
+    teacherDesc: 'He is also the founder of Schoolhouse.world, Khan Lab School and Khan World School, all nonprofits.',
   },
 ];
 
@@ -69,14 +69,14 @@ function loadTeachers(teacherInfoList) {
     teacher.classList.add('teacher-container');
     const info = teacherInfoList[i];
     const teacherItem = `
-      <div class="teacher-image">
-        <img src="${info.imageURL}" alt="teacher profile image" width="80px">
+      <div class='teacher-image'>
+        <img src='${info.imageURL}' alt='teacher profile image' width='80px'>
       </div>
-      <div class="teacher-sub">
-        <h3 class="teacher-name">${info.teacherName}</h3>
-        <span class="teacher-title">${info.teacherTitle}</span>
-        <div class="half-border-gray"></div>
-        <p class="teacher-description">${info.teacherDesc}</p>
+      <div class='teacher-sub'>
+        <h3 class='teacher-name'>${info.teacherName}</h3>
+        <span class='teacher-title'>${info.teacherTitle}</span>
+        <div class='half-border-gray'></div>
+        <p class='teacher-description'>${info.teacherDesc}</p>
       </div>
     `;
     teacher.innerHTML = teacherItem;
@@ -95,8 +95,8 @@ const teachersMore = document.querySelector('.teacher-more-text');
 const teachersMoreCont = document.querySelector('.teacher-more');
 const deskVer = document.querySelectorAll('.desktop-ver');
 teachersMore.addEventListener('click', () => {
-  for (let teacher of deskVer) {
-    teacher.style.display = 'flex';
+  for (let i = 0; i < deskVer.length; i += 1) {
+    deskVer[i].style.display = 'flex';
   }
   teachersMoreCont.style.display = 'none';
 });
